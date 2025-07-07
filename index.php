902297e4dd370be13ffdf77d15ceb79bb4a3d0dd
JoseBorda <bordajoseph4@gmail.com>
2025-07-06 20:43:02 -0500

    Se agrega diseno del login

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
+<?php
+include ("./database/conexion.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../estilo/style.css">
+    <link rel="stylesheet" href="./estilo/style.css">
     <title>Mini e-commerce</title>
 </head>
 <body>
     <form action="../admin/admin.login.php" method="post">
+        <!-- <img src="./imagenes/gif1.gif" alt="Imagen random" class="imagengif"> -->
         <div class="container-login">
-            <div class="container-main">
-                Usuario: <input type="text" name="login_user" id="login_user">
-            </div>
-            <div class="container-main">
-                Contrasenha: <input type="password" name="login_password" id="login_password">
-            </div>
-            <div class="container-button">
-                <input type="submit" value="Iniciar Sesion">
-            </div>
+            <div class="container-main">Usuario: </div><br>
+            <div class="container-input"><input type="text" name="login_user" id="login_user" class="form-input"></div><br>
+            <div class="container-main">Contrasenha: </div><br>
+            <div class="container-input"><input type="password" name="login_password" id="login_password" class="form-input"></div>
+            <div class="container-button"><input type="submit" value="Iniciar Sesion" class="button-primary"></div>
         </div>
     </form>
+
+     <!-- <p class="primer">Cuadro 1</p>
+     <p class="segundo">Cuadro 2</p>
+     <p class="tercero">Cuadro 3</p>
+     <p class="cuarto">Cuadro 4</p>
+     <img src="" alt="" class="quinto"> -->
 </body>
 </html>
